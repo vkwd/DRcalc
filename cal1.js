ec30bed8b4938bbfb659dcda1762b0c1363085c7
vkwd <vkwd@users.noreply.github.com>
2021-02-10 22:50:25 +0530

    refactored the code in files
    remoced all unrequired comments and added even listener to button click

--- cal1.js
+++ cal1.js
@@ -1,109 +1,29 @@
-//<script type="text/javascript">
+x = document.getElementById('err1');
 
-// function isValid() {
-//     if (isNaN(bp) || bp === '' || bp < 3500 || bp > 99999) {
-//         alert('enter valid basic pension');
-//         document.getElementById(
-//             'error'
-//         ).textContent = `Please enter a valid basic Pension number >3500 & < 99999`;
-//         return false;
-//     } else {
-//         alert(bp);
+function bpnsion() {
+    return parseInt(document.getElementById('basic_pension').value);
+}
 
-//         document.getElementById('error').textContent = '';
-//     }
-// }
-
-// function calculate() {
-//     //     //document.getElementById('DA_DR').value = 0;
-
-//     //     return;
-//     // }
-//     // if (!isValid(bp)) {
-//     //     const bp = parseInt(document.getElementById('basic_pension').value);
-//     const dr = 0.775;
-//     const drp = 0.759;
-//     //let gp = 0;
-//     //let gpp = 0;
-//     const bp = parseInt(document.getElementById('basic_pension').value);
-//     if (isNaN(bp) || bp == '' || bp < 3500 || bp > 99999) {
-//         // alert('enter valid basic pension');
-//         document.getElementById(
-//             'error'
-//         ).textContent = `Please enter a valid basic Pension number >3500 & < 99999`;
-//         return false;
-//     } else {
-//         document.getElementById('error').textContent = '';
-//     }
-
-//     //     return true;
-//     // }
-//     document.getElementById('DA_DR').value = dr;
-//     //const dr = 0.775;
-//     //const drp = 0.759;
-
-//     gp = bp + bp * dr;
-//     // alert(gp);
-
-//     document.getElementById('GPension').value = Math.ceil(gp);
-//     //alert(gp);
-
-//     gpp = bp + bp * drp;
-//     //alert(gpp);
-//     document.getElementById('GOL').value = Math.ceil(gp - gpp);
-
-//     function newFunction() {
-//         return true;
-//     }
-// }
-const bp = parseInt(document.getElementById('basic_pension').value);
-//alert(isValid(3200));
-//alert(bp);
-x = document.getElementById('error');
-
-function isValid() {
-    const bp = parseInt(document.getElementById('basic_pension').value);
-    //alert(bp);
-    if (isNaN(bp) || bp === '' || bp < 3500 || bp > 99999) {
-        //alert('enter valid basic pension');
-        // document.getElementById(
-        //     'error'
-        // ).innerHTML = `Please enter a valid basic Pension number >3500 & < 99999`;
-        x.innerHTML = `Please enter a valid basic Pension number >3500 & < 99999`;
+const calculate = () => {
+    const bp = bpnsion();
+    const dr = 0.852;
+    const drp = 0.775;
+    if (isNaN(bp) || bp == '' || bp < 3500 || bp > 99999) {
+        x.value = `enter Basic Pension >3500 or <99999`;
         return false;
     } else {
-        //     // bp = parseInt(document.getElementById('basic_pension').value);
-        //reset();
-        // document.getElementById('error').innerHTML.reset() = '';
-        //     // alert(bp);
-        //fun();
-        //newFunction();
-        // x.innerHTML.value.reset() = '';
-        calculate();
-
-        return true;
-        // }
+        x.value = '';
     }
+    document.getElementById('DA_DR').value = dr;
+    gp = bp + bp * dr;
 
-    // function newFunction() {
-    //     {
-    //         x.reset();
-    //     }
-    // }
+    document.getElementById('GPension').value = Math.ceil(gp);
 
-    function calculate() {
-        //isValid();
-        // const bp = parseInt(document.getElementById('basic_pension').value);
-        const dr = 0.852;
-        const drp = 0.775;
-        document.getElementById('DA_DR').value = dr;
-        gp = bp + bp * dr;
-        //alert(gp);
-        document.getElementById('GPension').value = Math.ceil(gp);
-        //alert(gp);
-        gpp = bp + bp * drp;
-        //alert(gpp);
-        document.getElementById('GOL').value = Math.ceil(gp - gpp);
-    }
-}
-// const x = document.getElementById('error');
+    gpp = bp + bp * drp;
+
+    document.getElementById('GOL').value = Math.ceil(gp - gpp);
+};
+
+const mybtn = document.querySelector('#btn');
+
+mybtn.addEventListener('click', calculate);
